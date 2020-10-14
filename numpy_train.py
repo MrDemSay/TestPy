2efd04a297b7da1f887e6389d0a6e5955aed9931
MrDemSay <dem_say@mail.ru>
2020-10-14 20:30:53 +0400

    upload. working func-"scan_on_zero"

--- numpy_train.py
+++ numpy_train.py
@@ -1,36 +1,54 @@
 array = [ [0, 2, 3],
-		  [5, 6, 7],
+		  [0, 6, 7],
 		  [45, 7, 2] ]
 
-		#чтобы в первой строке небыло элемента = 0
-		for i in range(3):
-			for j in range(3):
-				if array[0][0] == 0:
-					q = array[0]
-					array.remove(q)
-					array.append(q)
-
-			#print(array[i])
-
-
+#чтобы в первой строке небыло элемента = 0 (WORKING)
+def scan_on_zero(array):
+	for i in range(len(array)):
 		i = 0
-		j = 0
-		for i in range(3):
-			e = []
-			c = array[i+1]
-			q = -(array[i + 1][j] / array[i][j])
-			for j in range(3):
-				w = array[i][j] * q
-				e.append(w)
+		for j in range(len(array[i])):
+			if array[i][0] == 0:
+				q = array[i]
+				array.remove(q)
+				array.append(q)
+			else:
+				break
+		break
+scan_on_zero(array)
 
-			r = []
-			for y in range(3):
-				r.append(c[y] + e[y])
-			array.remove(array[i+1])
-			array.insert(i+1, r)
 
-		print(e)
-		print(array)
+# for i in range(3):
+# 	for j in range(3):
+# 		if array[i][0] == 0:
+#
+#
+
+
+
+	#print(array[i])
+#
+# a = 0
+# while a < 3:
+# 	i, j = a, a
+# 	a += 1
+#
+# 	e = []
+# 	for i in range(3):
+# 		e.clear()
+# 		c = array[i+1]
+# 		q = -(array[i + 1][j] / array[i][j])
+# 		for j in range(3):
+# 			w = array[i][j] * q
+# 			e.append(w)
+#
+# 		r = []
+# 		for y in range(3):
+# 			r.append(c[y] + e[y])
+# 		array.remove(array[i+1])
+# 		array.insert(i+1, r)
+#
+# 	print(e)
+# 	print(array)
 
 
 
