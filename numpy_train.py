a3783eec2a1cd581c3dbdab3603343ec4a64a42a
MrDemSay <dem_say@mail.ru>
2020-10-14 17:47:07 +0400

    Upload

--- numpy_train.py
+++ numpy_train.py
@@ -2,54 +2,35 @@ array = [ [0, 2, 3],
 		  [5, 6, 7],
 		  [45, 7, 2] ]
 
+		#чтобы в первой строке небыло элемента = 0
+		for i in range(3):
+			for j in range(3):
+				if array[0][0] == 0:
+					q = array[0]
+					array.remove(q)
+					array.append(q)
 
-#чтобы в первой строке небыло элемента = 0
-for i in range(3):
-	for j in range(3):
-		if array[0][0] == 0:
-			q = array[0]
-			array.remove(q)
-			array.append(q)
-
-	#print(array[i])
-
-
-i = 0
-j = 0
-for i in range(3):
-	e = []
-	c = array[i+1]
-	q = -(array[i + 1][j] / array[i][j])
-	for j in range(3):
-		w = array[i][j] * q
-		e.append(w)
-
-	r = []
-	for y in range(3):
-		r.append(c[y] + e[y])
-	array.remove(array[i+1])
-	array.insert(i+1, r)
-
-print(e)
-print(array)
+			#print(array[i])
 
 
+		i = 0
+		j = 0
+		for i in range(3):
+			e = []
+			c = array[i+1]
+			q = -(array[i + 1][j] / array[i][j])
+			for j in range(3):
+				w = array[i][j] * q
+				e.append(w)
 
-# i, j = 0, 0
-# z = []
-# for i in range(3):
-# 	for j in range(3):
-# 		w = 0
-# 		w = array[i][j] * -(array[i+1][j] / array[i][j])
-# 		z.append(w)
-#
-# 	e = []
-# 	for	t in range(3):
-# 		v = array[i+1]
-# 		e.append(v[t] + z[t])
-# 	array.remove(array[i+1])
-# 	array.insert(i+1, z)
-#
-#
-# 	print()
+			r = []
+			for y in range(3):
+				r.append(c[y] + e[y])
+			array.remove(array[i+1])
+			array.insert(i+1, r)
+
+		print(e)
+		print(array)
+
+
 
