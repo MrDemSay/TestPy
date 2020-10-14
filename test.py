a6ec785a0d11f5638163d88f326fa93166e9c28a
MrDemSay <dem_say@mail.ru>
2020-10-14 15:17:46 +0400

    First v

---
+++ test.py
@@ -0,0 +1,17 @@
+# a = [2, 3, 4]
+# b = [4, 5, 6]
+# e = [i * 2 for i in a]
+#
+# # for i in range(3):
+# #     r = a[i] + b[i]
+# #     e.append(r)
+#
+# print(e)
+y = 1
+e = []
+while y < 5:
+    a = int(input())
+    e.append(a)
+    y += 1
+print(e)
+
