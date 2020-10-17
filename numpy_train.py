e83a4ea05ce553298a007a0bcb01438213a3a5cf
MrDemSay <dem_say@mail.ru>
2020-10-17 19:23:02 +0400

    work version

--- numpy_train.py
+++ numpy_train.py
@@ -1,10 +1,9 @@
-import copy
 array = [[1, 5, 3], [2, 4, 12], [5, 6, 2]]
+b = [1, 3, 5]
 n = 3
 
 
 def scan_on_zero(a):
-
 	# чтобы в первой строке небыло элемента = 0 (WORKING)
 	for i in range(len(a)):
 		i = 0
@@ -21,23 +20,31 @@ def scan_on_zero(a):
 scan_on_zero(array)
 
 
+def forward_motion(a, b):
+	x = []
 
-def forward_motion(a):
-	i = 0
-	j = 0
-	m = 0
-	for j in range(n):
-		i = j
-		for k in range(j+1, n):
-			m = a[k][j] / a[i][j]
-			a[k][j] = a[k][j] - m * a[i][j]
-			for c in range(k, n):
-				a[k][c] = a[k][c] - m * a[k-1][c]
+	for k in range(0, n):
+		x.append(0)
+		for j in range(k+1, n):
+			d = a[j][k] / a[k][k]
+			for i in range(k, n):
+				a[j][i] = a[j][i] - d * a[k][i]
+			b[j] = b[j] - d * b[k]
+
+	for k in range(n-1, 0, -1):
+		d = 0
+		s = 0
+		for j in range(k, n):
+			s = a[k][j] * x[j]
+			d = d + s
+			x[k] = (b[k] - d) / a[k][k]
+	print('Result: ')
+
+	for i in range(0, n):
+		print('x [', i, '] = ', x[i])
 
 
-	return a
-
-print(forward_motion(array))
+forward_motion(array, b)
 
 
 
--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-import copy
-# a = [2, 3, 4]
-# b = [4, 5, 6]
-# e = [i * 2 for i in a]
-#
-# # for i in range(3):
-# #     r = a[i] + b[i]
-# #     e.append(r)
-#
-# print(e)
-a = [[1, 2, 3], [5, 6, 7], [45, 7, 2]]
-b = copy.deepcopy(a)
-for i in range(len(b)):
-    for j in range(len(b)):
-        b[i][j] = b[i][j] * 2
-print(b)
-print(a)
+a = []
+r = 2
+for i in range(3):
+    t = r * a[i]
+print(t)
